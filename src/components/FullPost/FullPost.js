6d522f4cec25adc5473298162e14813fc718afaa
Paige Niedringhaus <paigen11@gmail.com>
2019-07-28 19:00:27 -0400

    wip commit

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -21,11 +21,12 @@ class FullPost extends Component {
       if (
         !this.state.loadedPost ||
         (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
-      ) {
-        axios
-          .get(`/posts/${this.props.id}`)
-          .then(res => this.setState({ loadedPost: res.data }));
-      }
+      ) 
+      // {
+      //   axios
+      //     .get(`/posts/${this.props.id}`)
+      //     .then(res => this.setState({ loadedPost: res.data }));
+      // }
     }
   };
 
